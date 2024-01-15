c0f9e882184bdcabce86b9afae24e61c3a512488
Konstantin <kanstantsin.karakou@rk-software.net>
2024-01-15 11:30:43 +0100

    feat: initial project structure

---
+++ RK.Tychron.APIClient/TychronSMSAPIClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RK.Tychron.APIClient
+{
+    public class TychronSMSAPIClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public TychronSMSAPIClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
